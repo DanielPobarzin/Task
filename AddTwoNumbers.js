9ebf8e06a9a6fc2a00f0a44a098df765322d0e2b
Daniel <daniil.nyashka@gmail.com>
2024-01-09 11:43:33 +0300

    Исправил неточность

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -11,10 +11,10 @@ l1 = parseInt(l1.reverse().join(''))
 l2 = parseInt(l2.reverse().join(''))
 let l3 = l1 + l2
 let AddTwoNum =Array.from(String(l3), Number)
-return (AddTwoNum.reverse())
+return (AddTwoNum.reverse().join(''))
 }
 
-let line1 = [2,4,3]
-let line2 = [5,6,4]
+let line1 = [2,4,3,8]
+let line2 = [5,6,4,1]
 
 console.log(AddTwoNumbers(line1,line2))
